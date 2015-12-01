0b65c426ed3a6ca1ddfbd0c11509f26d47ee58f5
Sylvie Fiquet <sfiquetdev@gmail.com>
2015-12-01 11:37:28 +0000

    Fix: Clear the top of the player's head when on the top row
    When the player was on the top row, the top of its head persisted above the
    water tiles (that area is made of transparent bits in the tiles).

--- js/engine.js
+++ js/engine.js
@@ -128,6 +128,8 @@ var Engine = (function(global) {
             numCols = 5,
             row, col;
 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
          * portion of the "grid"
